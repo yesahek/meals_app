44fbba3cc3c6fd8102ed66b20da9bdba252a9a06
yesahek <yesahek@gmail.com>
2022-07-29 20:12:11 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/categories_meals_screen.dart';
-import 'package:meals_app/categories_screen.dart';
+import 'package:meals_app/screens/categories_meals_screen.dart';
+import 'package:meals_app/screens/categories_screen.dart';
 
 void main() => runApp(const MyApp());
 
---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import '../screens/categories_meals_screen.dart';
+
+class CategroyItem extends StatelessWidget {
+  final String title;
+  final Color color;
+  // ignore: prefer_typing_uninitialized_variables
+  final String id;
+  const CategroyItem({
+    Key? key,
+    required this.title,
+    required this.color,
+    required this.id,
+  }) : super(key: key);
+
+  void selectCategroy(BuildContext ctx) {
+    // Navigator.of(ctx).push(
+    //   MaterialPageRoute(
+    //     builder: (_) {
+    //       return CategoriesMealsScreen(title, color, id);
+    //     },
+    //   ),
+    // );
+    Navigator.of(ctx).pushNamed(
+      CategoriesMealsScreen.routeName,
+      arguments: {
+        'id': id,
+        'title': title,
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategroy(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Text(
+          title,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
